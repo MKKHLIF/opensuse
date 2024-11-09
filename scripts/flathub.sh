287b83862d32d401ddb2a280ff188a6b1759ce3c
Mayssam-Khlif <khlif.mk@proton.me>
2024-11-09 19:03:36 +0100

    remove redundent software installation

--- scripts/flathub.sh
+++ scripts/flathub.sh
@@ -8,10 +8,5 @@ fi
 
 echo "Enabling Flathub repository..."
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+echo "Flathub repository is enabled!"
 
-echo "Installing Flatpak packages..."
-while read -r package; do
-    flatpak install -y flathub "$package"
-done < ../packages/flatpak-packages.txt
-
-echo "Flatpak packages installed successfully."
