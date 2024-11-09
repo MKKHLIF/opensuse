d57075d69fe678fb1ff3cf1419604c944900b713
Mayssam-Khlif <khlif.mk@proton.me>
2024-11-09 17:27:42 +0100

    Require sudo for flatpak

--- scripts/flathub.sh
+++ scripts/flathub.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 set -e
 
+if [[ $(id -u) -ne 0 ]]; then
+    echo "This script must be run as root."
+    exit 1
+fi
+
 echo "Enabling Flathub repository..."
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 echo "Installing Flatpak packages..."
 while read -r package; do
     flatpak install -y flathub "$package"
-done < packages/flatpak-packages.txt
+done < ../packages/flatpak-packages.txt
 
 echo "Flatpak packages installed successfully."
